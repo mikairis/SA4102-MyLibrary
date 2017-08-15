9dbbb7ae4c30955071de7a17dc0e51bf330e4694
mikairis <thong.michael@gmail.com>
2017-08-15 11:39:57 +0800

    test

--- MyLibrary/Class1.cs
+++ MyLibrary/Class1.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace MyLibrary
 {
-    public class Class1
+    public static class Abc
     {
+        public static void ToDo()
+        {
+            Console.WriteLine("This is the library Abc.ToDo()");
+        }
     }
 }
